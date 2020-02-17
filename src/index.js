8d6ae99231d6093b210a21f1c4ed1024e64620e7
rubensojunior <rubensojunior6@gmail.com>
2020-02-17 15:45:23 -0300

    Configuring eslint, prettier and editorconfig

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+import './config/ReactotronConfig'
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>teste</Text>
+      </View>
+    )
+  }
+}
+
+console.tron.log('hello')
+
+export default App
