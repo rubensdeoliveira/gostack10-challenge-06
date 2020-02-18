ae98d9a0324b6a8a8fb85b5237c3ab1d989c8ce9
rubensojunior <rubensojunior6@gmail.com>
2020-02-18 09:07:52 -0300

    adding navigation

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,7 +32,7 @@ module.exports = {
         extensions: ['.jsx','js']
       }
     ],
-    'import/prefer-default-expert': 'off',
+    'import/prefer-default-export': 'off',
     semi: 'off'
   },
 };
