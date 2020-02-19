e39829a37faf250ef125ee57c962afeea9f8171a
rubensojunior <rubensojunior6@gmail.com>
2020-02-19 14:10:36 -0300

    adding refresh in list of starred projects

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { ActivityIndicator } from 'react-native'
 import PropTypes from 'prop-types'
 import {
   Container,
@@ -20,21 +21,50 @@ export default class User extends Component {
     super()
     this.state = {
       stars: [],
+      loading: true,
+      loadingMore: false,
+      refreshing: false,
+      page: 1,
     }
   }
 
-  async componentDidMount() {
+  componentDidMount() {
+    this.load()
+  }
+
+  refreshList = () => {
+    this.setState({ refreshing: true, stars: [] }, this.load)
+  }
+
+  loadMore = () => {
+    const { page } = this.state
+
+    const nextPage = page + 1
+
+    this.setState({ loadingMore: true }, () => this.load(nextPage))
+  }
+
+  load = async (page = 1) => {
+    const { stars } = this.state
     const { navigation } = this.props
     const user = navigation.getParam('user')
 
-    const response = await api.get(`/users/${user.login}/starred`)
+    const response = await api.get(`/users/${user.login}/starred`, {
+      params: { page },
+    })
 
-    this.setState({ stars: response.data })
+    this.setState({
+      stars: page >= 2 ? [...stars, ...response.data] : response.data,
+      page,
+      loading: false,
+      refreshing: false,
+      loadingMore: false,
+    })
   }
 
   render() {
     const { navigation } = this.props
-    const { stars } = this.state
+    const { stars, loading, loadingMore, refreshing } = this.state
 
     const user = navigation.getParam('user')
 
@@ -46,19 +76,29 @@ export default class User extends Component {
           <Bio>{user.bio}</Bio>
         </Header>
 
-        <Stars
-          data={stars}
-          keyExtractor={star => String(star.id)}
-          renderItem={({ item }) => (
-            <Starred>
-              <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
-              <Info>
-                <Title>{item.name}</Title>
-                <Author>{item.owner.login}</Author>
-              </Info>
-            </Starred>
-          )}
-        />
+        {loading ? (
+          <ActivityIndicator color="#7159c1" />
+        ) : (
+          <Stars
+            data={stars}
+            keyExtractor={star => String(star.id)}
+            onEndReachedThreshold={0.2}
+            onEndReached={this.loadMore}
+            onRefresh={this.refreshList}
+            refreshing={refreshing}
+            renderItem={({ item }) => (
+              <Starred>
+                <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+                <Info>
+                  <Title>{item.name}</Title>
+                  <Author>{item.owner.login}</Author>
+                </Info>
+              </Starred>
+            )}
+          />
+        )}
+
+        {loadingMore && <ActivityIndicator color="#7159c1" />}
       </Container>
     )
   }
